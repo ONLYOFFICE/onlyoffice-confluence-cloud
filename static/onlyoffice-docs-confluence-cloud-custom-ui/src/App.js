e59b3eba7b0511bec7d37bbf9c2af27beb4f475a
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2025-11-13 16:08:30 +0300

    feat: added fullPage module

---
+++ static/onlyoffice-docs-confluence-cloud-custom-ui/src/App.js
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from 'react';
+import { invoke } from '@forge/bridge';
+
+function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    invoke('getText', { example: 'my-invoke-variable' }).then(setData);
+  }, []);
+
+  return (
+    <div>
+      Tratatatata
+    </div>
+  );
+}
+
+export default App;
